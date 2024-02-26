96c9e01cc0353ae2990bd603b604e47a4d88c7d7
Amndeep Singh Mann <amann@mitre.org>
2024-02-26 16:40:38 -0500

    Sidestep some of the commonjs/esm issues and deal with linter complaints
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- db/umzug.ts
+++ db/umzug.ts
@@ -1,6 +1,6 @@
 import * as fs from "fs";
 import * as path from "path";
-import { Options, Sequelize, Model } from "sequelize";
+import { type Options, Sequelize, Model } from "sequelize";
 import * as dotenv from "dotenv";
 import { DateTime } from "luxon";
 import { Umzug, SequelizeStorage } from "../node_modules/umzug/lib/index";
