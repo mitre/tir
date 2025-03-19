7d76fccddeac9abfe7d9bc4b7877fa7d49d824c6
Mark Rivera <mcrivera@gmail.com>
2025-03-19 05:03:55 +0000

    Reverting accidental change to pages/home.vue
    Signed-off-by: Mark Rivera <mcrivera@gmail.com>

--- pages/home.vue
+++ pages/home.vue
@@ -1,49 +1,17 @@
 <template>
-  <div>
-    <div v-if="!isInitialized" class="flex h-screen items-center justify-center">
-      <p>Loading application data...</p>
+  <main class="">
+    <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
+      <!-- Your content -->
+      <DashBoardItems />
     </div>
-
-    <div v-else>
-      <main>
-        <div class="mx-auto max-w-7xl px-4 pb-12 sm:px-6 lg:px-8">
-          <DashBoardItems />
-        </div>
-      </main>
-    </div>
-  </div>
+  </main>
 </template>
 
-<script setup lang="ts">
-import { ref, onMounted } from "vue";
-import NavBar from "~/components/NavBar.vue";
-import DashBoardItems from "~/components/DashBoardItems.vue";
-import { useAliasStore } from "~~/stores/AliasStorage";
-import { useAlertsStore } from "~~/stores/AlertsStore";
+<script setup>
 import { useTestStore } from "~~/stores/HeaderValues";
-import { useRouter } from "vue-router";
+const store = useTestStore();
 
-const router = useRouter();
-const isInitialized = ref(false);
-
-const aliasStore = useAliasStore();
-const alertsStore = useAlertsStore();
-const testStore = useTestStore();
-
-onMounted(async () => {
-  await aliasStore.loadAliases();
-
-  const currentUser = await $fetch("/api/auth/currentUser");
-  testStore.changeUsername(currentUser?.email || "Welcome to TIR");
-
-  if (currentUser && currentUser.id) {
-    alertsStore.startPolling(currentUser.id);
-  } else {
-    console.warn("No valid user found; alerts polling not started.");
-  }
-
-  isInitialized.value = true;
-
-  router.push("/dashboard");
+onMounted(() => {
+  store.changeUsername("Welcome to TIR");
 });
 </script>
