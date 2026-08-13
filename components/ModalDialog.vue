1b51bf1e18e2cc345ef11dd787bfad6fe5e7214b
Mark Rivera <mcrivera@gmail.com>
2026-08-13 21:04:26 +0000

    fix(components): ignore Enter in the modal input while busy
    Signed-off-by: Mark Rivera <mcrivera@gmail.com>

--- components/ModalDialog.vue
+++ components/ModalDialog.vue
@@ -52,7 +52,7 @@
                       type="text"
                       :placeholder="placeholder"
                       class="mt-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-gray-700 dark:text-white dark:ring-gray-600 sm:text-sm"
-                      @keyup.enter="$emit('confirm', inputText)"
+                      @keyup.enter="!busy && $emit('confirm', inputText)"
                     />
                   </div>
                 </div>
